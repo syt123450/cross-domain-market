a7d314bf7bf388d8cd73be1e80cd63a8339c245d
syt123450 <421537891@qq.com>
2017-03-30 15:23:25 -0700

    add navbar storelist feature

--- slimSite/public/js/common.js
+++ slimSite/public/js/common.js
@@ -1,3 +1,24 @@
 /**
  * Created by ss on 2017/2/7.
  */
+
+function bindNav() {
+
+    $("nav>ul>li:eq(1)").hover(
+        function () {
+            $("nav>ul>li>ul:eq(0)").show();
+        },
+        function () {
+            $("nav>ul>li>ul:eq(0)").hide();
+        }
+    );
+
+    $("nav>ul>li:eq(3)").hover(
+        function () {
+            $("nav>ul>li>ul:eq(1)").show();
+        },
+        function () {
+            $("nav>ul>li>ul:eq(1)").hide();
+        }
+    );
+}
